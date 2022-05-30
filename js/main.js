53fc4a4677ec61391d62646f441266b9f15e4a97
uzielmf <uziel.munozf@gmail.com>
2022-05-30 11:45:30 -0500

    Agrege el ejercicio numero 3 (adivinar un numero entre el 1 y el 100)

--- js/main.js
+++ js/main.js
@@ -58,27 +58,27 @@ if ((num1 % 3) ==0)  {
 //Sumados 2 dan el
 
 
-let num1 = prompt("Ingrese el primer numero: ","")
-let num2 = prompt("Ingrese el segundo numero:","")
-let num3 = prompt("Ingrese el tercer numero:","") 
+//let num1 = prompt("Ingrese el primer numero: ","")
+//let num2 = prompt("Ingrese el segundo numero:","")
+//let num3 = prompt("Ingrese el tercer numero:","") 
 
-num1 = parseFloat (num1,10);
-num2 = parseFloat (num2,10);
-num3 = parseFloat (num3,10);
+//num1 = parseFloat (num1,10);
+//num2 = parseFloat (num2,10);
+//num3 = parseFloat (num3,10);
 
 
 
-if ((num1 + num2) === num3) {
-    alert ( num3 + " es la suma de el primer y segundo numero");
-}else if ((num2 + num3) === num1) {
-        alert ( num1 + " es la suma del segundo y el tercer numero");    
-}else if((num1 + num3) === num2)
- {
-    alert ( num2  + " es la suma del primer y tercer numero");
-}
-else {
-    alert ("ninguo es la suma de los otros dos");
-}
+//if ((num1 + num2) === num3) {
+ //   alert ( num3 + " es la suma de el primer y segundo numero");
+//}else if ((num2 + num3) === num1) {
+  //      alert ( num1 + " es la suma del segundo y el tercer numero");    
+//}else if((num1 + num3) === num2)
+// {
+//    alert ( num2  + " es la suma del primer y tercer numero");
+//}
+//else {
+//    alert ("ninguo es la suma de los otros dos");
+//}
 
 
 
@@ -96,6 +96,33 @@ if ((num1 % 2) ==0)  {
 
 
 
+//Adivina el numero
+function Adivinar() {
+
+    let superior = 100;
+    let inferior = 0;
+    let noEncontrado = true;
+    
+    while (noEncontrado) {
+        let mid = parseInt(inferior + ((superior - inferior)/ 2));
+        console.log (mid);
+        console.log(((superior-inferior)/ 2));
+    
+        if (((superior-inferior)/2)<1) {
+    noEncontrado = false;
+    alert("Tu numero es el " + (parseInt(mid)+1));
+    break;        
+        }
+        let res= confirm ("tu numero es menor o igual a "+ mid);
+        if (res) {
+            superior = mid;
+        } else {
+            inferior=mid;
+        }
+    }
+    }
+    
+    Adivinar();
 
 
 
